2a0caeb5b9d66afe05acb985d7df2af02d1efca2
John Elvis Arteaga Robles <john.arteagarobles@gmail.com>
2024-11-07 23:08:18 -0500

    act 23:08

--- build/lib/KalibrateLib/funciones.py
+++ build/lib/KalibrateLib/funciones.py
@@ -1,9 +1,6 @@
 import requests
 import json
 
-import requests
-import json
-
 def getToken(client_id,client_secret,scope,auth_url):
 
     post_body = {
@@ -16,3 +13,15 @@ def getToken(client_id,client_secret,scope,auth_url):
     response = requests.post(auth_url, data=post_body)
     token = response.json().get('access_token')
     return token
+
+def getData(urlApi,urlFilter,token):
+
+    authHeader = {"Authorization": "Bearer %s" %token, 'User-Agent': 'API-CLIENT'}
+    queryURL = f'https://eu2.data.kalibrate.cloud{urlApi}{urlFilter}'
+    response = requests.get(queryURL,headers=authHeader)
+
+    if response.status_code != 200:
+        return ""
+    else:
+        rawdata = json.loads(response.text)
+        return rawdata
