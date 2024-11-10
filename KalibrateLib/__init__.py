f35666dc2a49ea820374f08766e54f8905524c80
John Elvis Arteaga Robles <john.arteagarobles@gmail.com>
2024-11-10 14:22:26 -0500

    14:22

--- KalibrateLib/__init__.py
+++ KalibrateLib/__init__.py
@@ -1,9 +1,15 @@
 from .funciones import getToken
 
 def OSVersion():
-    det = """Version 1.3 =============
+    det = """Version 1.4 =============
     Libreria para agilizar el uso de las api de kalibrate
     USO:
     Consultar con John Arteaga
+
+    getColumnsSite _____
+    regresa 3 arreglos:
+        SiteInfo : data principal del site
+        CompetitorSites : informacion sobre 'competitor site' 
+        SiteGroupings : informacion sobre 'site grouping'
     """
     return det
